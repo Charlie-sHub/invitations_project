c3959f12bff9842611b2590634d3051b22926317
Carlos Mendez <carlosrafael-mg@hotmail.com>
2025-01-22 00:13:19 +0100

    Implementing the cart and home production repositories test
    Signed-off-by: Carlos Mendez <carlosrafael-mg@hotmail.com>

--- test/data/cart/production_cart_repository_test.dart
+++ test/data/cart/production_cart_repository_test.dart
@@ -1,9 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dartz/dartz.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:invitations_project/data/cart/production_cart_repository.dart';
+import 'package:invitations_project/data/core/failures/data_failure.dart';
 import 'package:invitations_project/data/core/misc/firebase/cloud_storage/cloud_storage_service.dart';
+import 'package:invitations_project/data/core/misc/firebase/firebase_helpers.dart';
+import 'package:invitations_project/data/core/misc/get_valid_invitation.dart';
+import 'package:invitations_project/data/core/models/invitation_dto.dart';
+import 'package:invitations_project/domain/core/entities/invitation.dart';
+import 'package:invitations_project/domain/core/misc/invitation_type.dart';
+import 'package:invitations_project/domain/core/validation/objects/future_date.dart';
+import 'package:invitations_project/domain/core/validation/objects/past_date.dart';
+import 'package:invitations_project/domain/core/validation/objects/title.dart';
+import 'package:invitations_project/domain/core/validation/objects/unique_id.dart';
 import 'package:logger/logger.dart';
+import 'package:mock_exceptions/mock_exceptions.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
 import 'production_cart_repository_test.mocks.dart';
 
@@ -14,18 +28,143 @@ import 'production_cart_repository_test.mocks.dart';
 void main() {
   late MockLogger mockLogger;
   late MockCloudStorageService mockCloudStorage;
-  late FakeFirebaseFirestore fakeFirebaseFirestore;
+  late FakeFirebaseFirestore fakeFirestore;
   late ProductionCartRepository repository;
+
+  final invitation = getValidInvitation();
+  final invitationId = invitation.id.getOrCrash();
 
   setUp(
     () {
       mockLogger = MockLogger();
       mockCloudStorage = MockCloudStorageService();
-      fakeFirebaseFirestore = FakeFirebaseFirestore();
+      fakeFirestore = FakeFirebaseFirestore();
       repository = ProductionCartRepository(
         mockLogger,
-        fakeFirebaseFirestore,
+        fakeFirestore,
         mockCloudStorage,
+      );
+    },
+  );
+
+  group(
+    'saveInvitation',
+    () {
+      test(
+        'should save an invitation successfully',
+        () async {
+          // Arrange
+
+          // Act
+          final result = await repository.saveInvitation(invitation);
+          final invitationDoc =
+              await fakeFirestore.invitationCollection.doc(invitationId).get();
+
+          // Assert
+          expect(result.isRight(), true);
+          expect(invitationDoc.exists, true);
+        },
+      );
+
+      test(
+        'should return a DataFailure on FirebaseException',
+        () async {
+          // Arrange
+          final doc = fakeFirestore.collection("invitations").doc(invitationId);
+          whenCalling(
+            Invocation.method(
+              #set,
+              null,
+            ),
+          ).on(doc).thenThrow(
+                FirebaseException(plugin: 'firestore'),
+              );
+
+          // Act
+          final result = await repository.saveInvitation(invitation);
+
+          // Assert
+          expect(result.isLeft(), true);
+          expect(result.leftMap((failure) => failure is DataFailure), true);
+          verify(mockLogger.e(any)).called(1);
+        },
+      );
+    },
+  );
+
+  group(
+    'deleteInvitation',
+    () {
+      test(
+        'should delete an invitation successfully',
+        () async {
+          // Arrange
+          final doc = fakeFirestore.invitationCollection.doc(invitationId);
+          await doc.set(InvitationDto.fromDomain(invitation));
+
+          // Act
+          final result = await repository.deleteInvitation(invitation.id);
+
+          // Assert
+          expect(result.isRight(), true);
+          final invitationDoc = await doc.get();
+          expect(invitationDoc.exists, false);
+        },
+      );
+
+      test(
+        'should return a DataFailure on FirebaseException',
+        () async {
+          // Arrange
+          final doc = fakeFirestore.collection("invitations").doc(invitationId);
+          whenCalling(
+            Invocation.method(
+              #delete,
+              null,
+            ),
+          ).on(doc).thenThrow(
+                FirebaseException(plugin: 'firestore'),
+              );
+
+          // Act
+          final result = await repository.deleteInvitation(invitation.id);
+
+          // Assert
+          expect(result.isLeft(), true);
+          expect(result.leftMap((failure) => failure is DataFailure), true);
+          verify(mockLogger.e(any)).called(1);
+        },
+      );
+    },
+  );
+
+  // TODO: Update when purchase has been properly implemented
+  group(
+    'purchase',
+    skip: true,
+    () {
+      test(
+        'should return right(unit) when purchase is successful',
+        () async {
+          // Act
+          final result = await repository.purchase();
+
+          // Assert
+          expect(result, right(unit));
+        },
+      );
+
+      test(
+        'should return a DataFailure on error',
+        () async {
+          // Act
+          final result = await repository.purchase();
+
+          // Assert
+          expect(result.isLeft(), true);
+          expect(result.leftMap((failure) => failure is DataFailure), true);
+          verify(mockLogger.e(any)).called(1);
+        },
       );
     },
   );
--- test/data/home/production_home_repository_test.dart
+++ test/data/home/production_home_repository_test.dart
@@ -1,8 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:invitations_project/data/core/failures/data_failure.dart';
+import 'package:invitations_project/data/core/misc/firebase/firebase_helpers.dart';
+import 'package:invitations_project/data/core/misc/get_valid_invitation.dart';
+import 'package:invitations_project/data/core/models/invitation_dto.dart';
 import 'package:invitations_project/data/home/production_home_repository.dart';
 import 'package:logger/logger.dart';
+import 'package:mock_exceptions/mock_exceptions.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
 import 'production_home_repository_test.mocks.dart';
 
@@ -12,6 +19,11 @@ void main() {
   late FakeFirebaseFirestore fakeFirebaseFirestore;
   late ProductionHomeRepository repository;
 
+  final invitations = [
+    getValidInvitation(),
+    getValidInvitation(),
+  ];
+
   setUp(
     () {
       mockLogger = MockLogger();
@@ -20,6 +32,59 @@ void main() {
         mockLogger,
         fakeFirebaseFirestore,
       );
+    },
+  );
+
+  test(
+    'should return a list of example invitations when there are some',
+    () async {
+      // Arrange
+      for (final invitation in invitations) {
+        final dto = InvitationDto.fromDomain(invitation);
+        await fakeFirebaseFirestore.invitationCollection.doc(dto.id).set(dto);
+      }
+
+      // Act
+      final result = await repository.getExampleInvitations();
+
+      // Assert
+      expect(result.isRight(), true);
+      expect(result.getOrElse(() => []), invitations);
+    },
+  );
+
+  test(
+    'should return an empty list when there are no example invitations',
+    () async {
+      // Act
+      final result = await repository.getExampleInvitations();
+
+      // Assert
+      expect(result.isRight(), true);
+      expect(result.getOrElse(() => []), []);
+    },
+  );
+
+  test(
+    'should return a DataFailure on FirebaseException',
+    () async {
+      final collection = fakeFirebaseFirestore.collection('invitations');
+      whenCalling(
+        Invocation.method(
+          #get,
+          null,
+        ),
+      ).on(collection).thenThrow(
+            FirebaseException(plugin: 'firestore'),
+          );
+
+      // Act
+      final result = await repository.getExampleInvitations();
+
+      // Assert
+      expect(result.isLeft(), true);
+      expect(result.leftMap((failure) => failure is DataFailure), true);
+      verify(mockLogger.e(any)).called(1);
     },
   );
 }
